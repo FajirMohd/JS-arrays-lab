a4b22e87378e26bfe9e29ab8883fa09f0276ce01
FajirMohd <fajer_mohd@outlook.com>
2024-11-06 15:50:18 +0300

    affe85da06c715a50fb125759be0442186aafcaa

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("hello")
+const foods = ["pizza","cheesebutger"];  
+console.log('Exercise 1 result:', foods);
+console.log('Exercise 2 result:', foods);
+foods.unshift("tacos")
+console.log('Exercise 3 result:', foods)
+let mfavfood = 'pizza',
+    console.log '$(index) => $(number')
+})
+    
+let favFood = ("pizza"[1])
+console.log('Exercise 4 result:', favFood);
+
---
+++ script.js
@@ -0,0 +1,8 @@
+const person = {
+    name: "Fajer",
+    age: 30,
+    height: 167,
+    hasMustache: false,
+    hasBeard: false,
+    }
+
